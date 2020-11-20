045ea265a0c997ab02fdb04eff5268218b7d8dcb
Xiongfei Zhang <xzhang41@uncc.edu>
2020-11-20 00:02:46 -0500

    added questions and career descriptions

--- javascripts.js
+++ javascripts.js
@@ -29,59 +29,143 @@ function addLogical( amount)
 	logicalValue = 0;
  }
 
-//function that check whether quiz answer input is correct 
+//function that add values based off of answers
 function grade1(){
 	if(q1a.checked == true){
-		addInductive(00);
+		addLogical(0);
+		addAbstract(0);
+		addInductive(10);
 	}
 	else if(q1b.checked == true){
+		addLogical(5);
+		addAbstract(5);
 		addInductive(10);
 	}
 	else if(q1c.checked == true){
-		addInductive(20);
+		addLogical(5);
+		addAbstract(5);
+		addInductive(5);
 	}
 	else if(q1d.checked == true){
-		addInductive(30);
+		addLogical(5);
+		addAbstract(5);
+		addInductive(0);
 	}
 	else if(q1e.checked == true){
-		addInductive(40);
+		addLogical(0);
+		addAbstract(10);
+		addInductive(0);
 	}
 }
 function grade2(){
 	if(q2a.checked == true){
-		addAbstract(00);
+		addLogical(0);
+		addAbstract(10);
+		addInductive(5);
 	}
 	else if(q2b.checked == true){
+		addLogical(5);
 		addAbstract(10);
+		addInductive(5);
 	}
 	else if(q2c.checked == true){
-		addAbstract(20);
+		addLogical(5);
+		addAbstract(5);
+		addInductive(5);
 	}
 	else if(q2d.checked == true){
-		addAbstract(30);
+		addLogical(10);
+		addAbstract(5);
+		addInductive(10);
 	}
 	else if(q2e.checked == true){
-		addAbstract(40);
+		addLogical(15);
+		addAbstract(5);
+		addInductive(10);
 	}
 }
 function grade3(){
 	if(q3a.checked == true){
-		addLogical(00);
+		addLogical(10);
+		addAbstract(0);
+		addInductive(5);
 	}
 	else if(q3b.checked == true){
-		addLogical(10);
+		addLogical(5);
+		addAbstract(5);
+		addInductive(5);
 	}
 	else if(q3c.checked == true){
-		addLogical(20);
+		addLogical(5);
+		addAbstract(5);
+		addInductive(5);
 	}
 	else if(q3d.checked == true){
-		addLogical(30);
+		addLogical(0);
+		addAbstract(10);
+		addInductive(5);
 	}
 	else if(q3e.checked == true){
-		addLogical(40);
+		addLogical(0);
+		addAbstract(10);
+		addInductive(5);
 	}
 }
 
+function grade4(){
+	if(q4a.checked == true){
+		addLogical(0);
+		addAbstract(10);
+		addInductive(0);
+	}
+	else if(q4b.checked == true){
+		addLogical(0);
+		addAbstract(5);
+		addInductive(5);
+	}
+	else if(q4c.checked == true){
+		addLogical(10);
+		addAbstract(5);
+		addInductive(5);
+	}
+	else if(q4d.checked == true){
+		addLogical(10);
+		addAbstract(0);
+		addInductive(5);
+	}
+	else if(q4e.checked == true){
+		addLogical(15);
+		addAbstract(0);
+		addInductive(10);
+	}
+}
+function grade5(){
+	if(q5a.checked == true){
+		addLogical(0);
+		addAbstract(10);
+		addInductive(5);
+	}
+	else if(q5b.checked == true){
+		addLogical(0);
+		addAbstract(5);
+		addInductive(5);
+	}
+	else if(q5c.checked == true){
+		addLogical(10);
+		addAbstract(5);
+		addInductive(5);
+	}
+	else if(q5d.checked == true){
+		addLogical(10);
+		addAbstract(0);
+		addInductive(5);
+	}
+	else if(q5e.checked == true){
+		addLogical(15);
+		addAbstract(0);
+		addInductive(10);
+	}
+}
 
 //grade function that runs all the others and outputs response
 function grade(){
@@ -89,17 +173,25 @@ function grade(){
 	grade1();
 	grade2();
 	grade3();
+	grade4();
+	grade5();
 	
 	//finds best career
+	//alert("inductive " + inductiveValue + "\nabstract: " + abstractValue + "\nlogical: " + logicalValue);
 	var bestCareer = calculateBestCareer(inductiveValue, abstractValue, logicalValue);
+	
+	//hides questions 
+	document.getElementById("listQuestions").style.display = "none";
 
-	//display career name and photo
+
+	//display career info
+
 	document.getElementById("resultsTitle").innerHTML = "You seem fit to become a : ";
 	document.getElementById("resultsCareer").innerHTML = bestCareer.careerName;
 	document.getElementsByClassName("quizButtons")[0].style.visibility = 'hidden';
 	document.getElementById("careerDescription").innerHTML = bestCareer.description;
-
 	document.careerImage.src= bestCareer.imageLink;
+
 }
 
 
@@ -141,6 +233,7 @@ function calculateBestCareer(  inductiveV, abstractV, logicalV){
 
 
 /*
+for next sprint:
 Cognitive Abilities influence the acquisition and application of knowledge in problem solving.
 Physical Abilities influence strength, endurance, flexibility, balance and coordination.
 Psychomotor Abilities influence the capacity to manipulate and control objects
@@ -151,64 +244,64 @@ Sensory Abilities influence visual, auditory and speech perception.
 var mechanic = {
 	careerName		: "Mechanic",
 	inductiveValue	: 30,
-	logicalValue	: 30,
-	abstractValue	: 30,
+	abstractValue	: 15,
+	logicalValue	: 50,
 	imageLink: 'img/mechanic.jpg',
-	description: "Description of career here"	 
+	description: " Mechanics are professionals known for inspecting, repairing, and performing high level maintenance on machines and vehicles. Mechanics often practice problem solving skills, working in teams, and thrive in environments with good communication."	 
 };
 
 var putech = {
 	careerName		: "Power Utility Tech",
-	inductiveValue	: 32,
-	logicalValue	: 55,
-	abstractValue	: 12,
+	inductiveValue	: 30,
+	abstractValue	: 30,
+	logicalValue	: 30,
 	imageLink: 'img/putech.jpg',
-	description: "Description of career here"	 
+	description: "Power Utility technicians monitor energy output for utility companies. Their tasks vary from regulating energy distribution and operating power equipment to measuring power flow. Utility Technicians practice critical thinking and working in small groups while traveling to different locations of the city daily."	 
 };
 
 var mason = {
 	careerName		: "Mason",
-	inductiveValue	: 27,
-	logicalValue	: 11,
-	abstractValue	: 36,
+	inductiveValue	: 35,
+	abstractValue	: 20,
+	logicalValue	: 40,
 	imageLink: 'img/mason.jpg',
-	description: "Description of career here"	 
+	description: "Masons are a type of construction worker that are responsible for building structures such as walkways, chimneys, and etc. They are expected to carry heavy objects as well as restore old and damaged structures. Since safety in the work environment is their main concern, they must carefully plan out everything before physically following through their plan."	 
 };
 
-var carpenter = {
-	careerName		: "Carpenter",
-	inductiveValue	: 10,
-	logicalValue	: 10,
-	abstractValue	: 10,
-	imageLink: 'img/carpenter.jpg',
-	description: "Description of career here"	 
+var photographer = {
+	careerName		: "Photographer",
+	inductiveValue	: 20,
+	abstractValue	: 40,
+	logicalValue	: 20,
+	imageLink: 'img/photographer.jpg',
+	description: "Photographers are often free lance workers hired by contractors to take professional photos of various events, from sports, to celebrations. They can also  creating their own art through photography"
 };
 
-var construction = {
-	careerName		: "Construction Worker",
-	inductiveValue	: 22,
-	logicalValue	: 11,
-	abstractValue	: 51,
-	imageLink: 'img/construction.jpg',
-	description: "Description of career here"	 
+var teacher = {
+	careerName		: "Teacher",
+	inductiveValue	: 35,
+	abstractValue	: 15,
+	logicalValue	: 55,
+	imageLink: 'img/teacher.jpg',
+	description: "Teachers are leaders who specialize in communicating hard topics to students in an easy to understand way by using examples. They must also be good listeners to understand the problems or requests students might have."	 
 };
 
 var nurse = { 
 	careerName		: "Nurse",
-	inductiveValue	: 40,
-	logicalValue	: 20,
-	abstractValue	: 20,
+	inductiveValue	: 25,
+	abstractValue	: 15,
+	logicalValue	: 35,
 	imageLink: 'img/nurse.jpg',
-	description: "Description of career here"	 
+	description: "Nurses oversee doctors by assisting them and the patients that are hospitalized. They must have great communication skills while also being able to follow required protocol."	 
 };
 
 var electrician = {
 	careerName		: "Electrician",
-	inductiveValue	: 10,
-	logicalValue	: 30,
-	abstractValue	: 10,
+	inductiveValue	: 25,
+	abstractValue	: 40,
+	logicalValue	: 20,
 	imageLink: 'img/electrician.jpg',
-	description: "Description of career here"	 
+	description: "Electricians oversee installing, and maintaining, and repairing electrical wiring, any equipment, and fixtures. They are great problem solvers by by using the many skills they have to create new solutions based on their current location."	 
 };
-var careerList = [mechanic, putech, mason, carpenter, construction, nurse, electrician];
+var careerList = [mechanic, putech, mason, photographer, teacher, nurse, electrician];
 
