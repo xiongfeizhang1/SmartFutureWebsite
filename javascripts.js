a7b8dba31d8d6e1c05434d7885be0ef09244288f
Xiongfei Zhang <xzhang41@uncc.edu>
2020-12-12 20:54:49 -0500

    added saved cookies to assessment and ability to view previous test results

--- javascripts.js
+++ javascripts.js
@@ -1,5 +1,79 @@
 
 
+
+
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initAutocomplete() {
+	const map = new google.maps.Map(document.getElementById("map"), {
+	  center: { lat: -33.8688, lng: 151.2195 },
+	  zoom: 13,
+	  mapTypeId: "roadmap",
+	});
+	// Create the search box and link it to the UI element.
+	const input = document.getElementById("pac-input");
+	const searchBox = new google.maps.places.SearchBox(input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener("bounds_changed", () => {
+	  searchBox.setBounds(map.getBounds());
+	});
+	let markers = [];
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener("places_changed", () => {
+	  const places = searchBox.getPlaces();
+  
+	  if (places.length == 0) {
+		return;
+	  }
+	  // Clear out the old markers.
+	  markers.forEach((marker) => {
+		marker.setMap(null);
+	  });
+	  markers = [];
+	  // For each place, get the icon, name and location.
+	  const bounds = new google.maps.LatLngBounds();
+	  places.forEach((place) => {
+		if (!place.geometry) {
+		  console.log("Returned place contains no geometry");
+		  return;
+		}
+		const icon = {
+		  url: place.icon,
+		  size: new google.maps.Size(71, 71),
+		  origin: new google.maps.Point(0, 0),
+		  anchor: new google.maps.Point(17, 34),
+		  scaledSize: new google.maps.Size(25, 25),
+		};
+		// Create a marker for each place.
+		markers.push(
+		  new google.maps.Marker({
+			map,
+			icon,
+			title: place.name,
+			position: place.geometry.location,
+		  })
+		);
+  
+		if (place.geometry.viewport) {
+		  // Only geocodes have viewport.
+		  bounds.union(place.geometry.viewport);
+		} else {
+		  bounds.extend(place.geometry.location);
+		}
+	  });
+	  map.fitBounds(bounds);
+	});
+  }
+  
+
+var careerResult;
 var cognitiveValue = 0;
 var physicalValue = 0;
 var psychomotorValue = 0;
@@ -219,6 +293,9 @@ function grade(){
 	document.getElementById("careerDescription").innerHTML = bestCareer.description;
 	document.careerImage.src= bestCareer.imageLink;
 
+	//saves result career to cookies
+	setCookie("career", bestCareer.career, 10);
+
 }
 
 /*
@@ -280,6 +357,7 @@ Sensory Abilities influence visual, auditory and speech perception.
 
 
 var mechanic = {
+	career			: "mechanic",
 	careerName		: "Mechanic",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -290,6 +368,7 @@ var mechanic = {
 };
 
 var putech = {
+	career			: "putech",
 	careerName		: "Power Utility Tech",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -300,6 +379,7 @@ var putech = {
 };
 
 var mason = {
+	career			: "mason",
 	careerName		: "Mason",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -310,6 +390,7 @@ var mason = {
 };
 
 var photographer = {
+	career			: "photographer",
 	careerName		: "Photographer",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -320,6 +401,7 @@ var photographer = {
 };
 
 var teacher = {
+	career			: "nurse",
 	careerName		: "Teacher",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -329,7 +411,8 @@ var teacher = {
 	description: "Teachers are leaders who specialize in communicating hard topics to students in an easy to understand way by using examples. They must also be good listeners to understand the problems or requests students might have."	 
 };
 
-var nurse = { 
+var nurse = {
+	career			: "nurse", 
 	careerName		: "Nurse",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -340,6 +423,7 @@ var nurse = {
 };
 
 var electrician = {
+	career			: "electrician",
 	careerName		: "Electrician",
 	cognitiveValue	: 25,
 	physicalValue	: 40,
@@ -350,3 +434,59 @@ var electrician = {
 };
 var careerList = [mechanic, putech, mason, photographer, teacher, nurse, electrician];
 
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + "samesite=Lax; " + expires + ";path=/";
+  }
+  function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+	  var c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+function checkCookie() {
+	var career = getCookie(career);
+	if (career != " ") {
+	 alert("You have no saved career results!");
+	} else {
+		//display career info
+		var savedCareer = getCookie("career");
+		//hides questions 
+		document.getElementById("listQuestions").style.display = "none";
+
+
+		//display career info
+
+		document.getElementById("resultsTitle").innerHTML = "You seem fit to become a : ";
+		document.getElementById("resultsCareer").innerHTML = this[savedCareer].careerName;
+		document.getElementsByClassName("quizButtons")[0].style.visibility = 'hidden';
+		document.getElementById("careerDescription").innerHTML = this[savedCareer].description;
+		document.careerImage.src= this[savedCareer].imageLink;
+
+
+	  }
+  }
+  
+	/*
+  //when the assessment page loads checks for saved cookies
+  if (window.location.href.match('assessment.html')){
+	var hasCookies = checkCookie();
+	if(hasCookies = true)
+	
+	{
+
+	}
+}
+*/
